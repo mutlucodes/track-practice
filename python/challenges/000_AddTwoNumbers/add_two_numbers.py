72d83748624d58f3a4e6829452d5153718df6bf7
Omar Qaddoumi <omarqaddoumi@gmail.com>
2023-12-24 02:35:03 +0000

    build(python): setup initial project structure

---
+++ python/challenges/000_AddTwoNumbers/add_two_numbers.py
@@ -0,0 +1,10 @@
+def add_two_numbers(a, b):
+    """
+    Adds two numbers and returns the result.
+    Parameters:
+    a (int/float): The first number to be added.
+    b (int/float): The second number to be added.
+    Returns:
+    int/float: The sum of the two numbers.
+    """
+    return a + b
