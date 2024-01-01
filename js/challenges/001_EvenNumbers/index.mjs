c1ccc9a02868810fa370c6c10737e7acb8b8a87a
Mutlu Aydin <mutluaydin.m@gmail.com>
2024-01-01 15:30:49 -0800

    Add JS solution for 001_EvenNumbers

---
+++ js/challenges/001_EvenNumbers/index.mjs
@@ -0,0 +1,14 @@
+export default function removeEvenNumbers(nums) {
+
+    const oddNumbers = [];
+    for (let i = 0; i < nums.length; i++) {
+        if (!Number.isInteger(nums[i])) {
+            throw new Error("Array must contain only integers");
+        }
+        if (nums[i] % 2 !== 0) {
+            oddNumbers.push(nums[i]);
+        }
+    }
+    return oddNumbers;
+}
+
