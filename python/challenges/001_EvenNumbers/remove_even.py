4ea88bb8a1e51c2de55dfc022e5a4bb9522c81e0
Mutlu Aydin <mutluaydin.m@gmail.com>
2024-01-05 08:27:28 -0800

    Fix lint errors

--- python/challenges/001_EvenNumbers/remove_even.py
+++ python/challenges/001_EvenNumbers/remove_even.py
@@ -1,5 +1,14 @@
 # Remove Even Numbers Function
 def remove_even_numbers(nums):
+    """
+    Remove all even numbers from a list and return a new list containing only the odd numbers.
+
+    Args:
+    nums (list): A list of integers.
+
+    Returns:
+    list: A list containing only the odd numbers from the original list.
+    """
     odd_numbers = []
     for i in nums:
         if i%2 != 0:
