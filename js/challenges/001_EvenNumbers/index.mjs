6969d6aac3eacaf78168e96fbad85c0e39e1143e
Mutlu Aydin <mutluaydin.m@gmail.com>
2024-01-05 08:22:48 -0800

    Fix lint errors

--- js/challenges/001_EvenNumbers/index.mjs
+++ js/challenges/001_EvenNumbers/index.mjs
@@ -1,9 +1,9 @@
+// Remove Even Numbers Function
 export default function removeEvenNumbers(nums) {
-
     const oddNumbers = [];
     for (let i = 0; i < nums.length; i++) {
         if (!Number.isInteger(nums[i])) {
-            throw new Error("Array must contain only integers");
+            throw new Error('Array must contain only integers');
         }
         if (nums[i] % 2 !== 0) {
             oddNumbers.push(nums[i]);
@@ -11,4 +11,3 @@ export default function removeEvenNumbers(nums) {
     }
     return oddNumbers;
 }
-
--- python/challenges/001_EvenNumbers/remove_even.py
+++ python/challenges/001_EvenNumbers/remove_even.py
@@ -1,8 +1,7 @@
+# Remove Even Numbers Function
 def remove_even_numbers(nums):
     odd_numbers = []
     for i in nums:
         if i%2 != 0:
             odd_numbers.append(i)
-
-    
     return odd_numbers
